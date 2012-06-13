1c2dc88acecfdbc4dadcc23f21d75608add2a170
flopp <fpigorsch@gmail.com>
2012-06-14 00:15:02 +0200

    added actual python file

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 ocdl.py
 =======
 
-Python script for downloading opencacvhng.de's "saved queries"
+Python script for downloading opencaching.de's "saved queries"
+
+
+Usage: ocdl.py [options] [ID...]
+
+Options:
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+  -c CONFIGDIR, --configdir=CONFIGDIR
+                        specify directory for config files (cookies, login
+                        information); default is '~/.ocdl'
+  -l, --list            list available queries, do not download them
+  -n STYLE, --name=STYLE
+                        select naming style of downloaded queries, STYLE={ ID,
+                        ID+DATE, NAME, NAME+DATE }, default is 'ID'
+  -d TARETDIR, --dir=TARETDIR
+                        select target directory for downloaded queries;
+                        default is the current directory
+  -v, --verbose         print verbose status messages
+
+
+ocdl.py logs in to opencaching.de using login credentials which are stored
+in '~/.ocdl/config.txt' (the actual directory is configurable using the
+'--configdir' command line option).
+Make sure the directory '~/.ocdl/' (or whatever you chose by
+'--configdir') exists and contains the file 'config.txt' with the
+following two lines:
+
+OCDE_LOGIN='username'
+OCDE_PASSWORD='password'
+
+where username and password are your login credentials for opencaching.de
+
+Moreover, ocdl.py stores the login cookie 'cookies.txt' in the selected  
+config directory for future logins.
---
+++ ocdl.py
@@ -0,0 +1,329 @@
+#!/usr/bin/python
+
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Author: Flopp <mail@flopp-caching.de>
+#
+
+import cookielib
+import urllib
+import urllib2
+import os
+import re
+import sys
+from optparse import OptionParser, OptionGroup, OptionValueError
+from datetime import date
+
+OCDLPY_VERSION="ocdl.py v0.1 2012-06-13"
+
+def naming_style_callback( option, opt_str, value, parser ) :
+	if not value in [ "ID", "ID+DATE", "NAME", "NAME+DATE" ] :
+		raise OptionValueError( "bad STYLE value for option '%s': '%s'" % ( opt_str, value ) )
+	setattr( parser.values, option.dest, value )
+
+additional_help="""
+ocdl.py logs in to opencaching.de using login credentials which are stored |
+| in '~/.ocdl/config.txt' (the actual directory is configurable using the   |
+| '--configdir' command line option).                                       |
+| Make sure the directory '~/.ocdl/' (or whatever you chose by              |
+| '--configdir') exists and contains the file 'config.txt' with the         |
+| following two lines:                                                      |
+|                                                                           |
+| OCDE_LOGIN='username'                                                     |
+| OCDE_PASSWORD='password'                                                  |
+|                                                                           |
+| where username and password are your login credentials for opencaching.de |
+|                                                                           |
+| Moreover, ocdl.py stores the login cookie 'cookies.txt' in the selected   |
+| config directory for future logins.                                       |
+"""
+
+parser = OptionParser( usage = "%prog [options] [ID...]", version = OCDLPY_VERSION, epilog = additional_help )
+parser.add_option( "-c", "--configdir", metavar="CONFIGDIR", dest="config_dir", default="~/.ocdl",
+                   help="specify directory for config files (cookies, login information); default is '~/.ocdl'" )
+parser.add_option( "-l", "--list", action="store_true", dest="list_only", default=False, 
+				   help="list available queries, do not download them" )
+parser.add_option( "-n", "--name", metavar="STYLE", 
+				   action="callback", callback=naming_style_callback, type="string", dest="naming_style", default="ID",
+				   help="select naming style of downloaded queries, STYLE={ ID, ID+DATE, NAME, NAME+DATE }, default is 'ID'" )
+parser.add_option( "-d", "--dir", metavar="TARETDIR", dest="target_dir", default=".",
+				   help="select target directory for downloaded queries; default is the current directory" )
+parser.add_option( "-v", "--verbose", action="store_true", dest="be_verbose", default=False,
+				   help="print verbose status messages" )
+(options, args) = parser.parse_args()
+
+
+cookie_jar = cookielib.LWPCookieJar()
+opener = urllib2.build_opener( urllib2.HTTPCookieProcessor( cookie_jar ) )
+urllib2.install_opener( opener )
+
+headers =  {'User-agent' : 'OCDL.py'}
+
+def create_directory( d ) :
+	if not os.path.isdir( d ) :
+		if options.be_verbose :
+			print "  -- dir '%s' does not exist" % (d)
+			print "  -- trying to create dir '%s'" % (d)
+		
+		try :
+			os.makedirs( d )
+		except OSError, e :
+			print "ERROR: cannot create dir '%s'" % (d)
+			print "  -- ", e
+			raise Exception
+
+def load_credentials( filename ) :
+	if not os.path.isfile( filename ) :
+		if options.be_verbose :
+			print "  -- file '%s' does not exist" % (filename)
+			return (None,None)
+	
+	try :
+		f = open( filename,"r" )
+		lines = f.readlines()
+		
+		# OCDE_LOGIN='username'
+		# OCDE_PASSWORD='password'
+		
+		login=None
+		password=None
+		
+		for line in lines :
+			line=line.strip()
+			
+			if line == "" :
+				continue
+			elif line.startswith( "#" ) :
+				continue
+			elif line.startswith( "OCDE_LOGIN='" ) :
+				if not( login is None ) :
+					if options.be_verbose :
+						print "  --  WARNING: duplicate 'OCDE_LOGIN' lines in file '%s'" % (filename)
+					return (None,None)
+				if not( len(line)>12 ) or not( line.endswith("'") ) :
+					if options.be_verbose :
+						print "  -- WARNING: bad 'OCDE_LOGIN' line in file '%s': %s" % (filename, line)
+					return (None,None)
+				login = line[12:-1]
+			elif line.startswith( "OCDE_PASSWORD='" ) :
+				if not( password is None ) :
+					if options.be_verbose :
+						print "  -- WARNING: duplicate 'OCDE_PASSWORD' lines in file '%s'" % (filename)
+					return (None,None)
+				if not( len(line)>15 ) or not( line.endswith("'") ) :
+					if options.be_verbose :
+						print "  -- WARNING: bad 'OCDE_PASSWORD' line in file '%s': %s" % (filename, line)
+					return (None,None)
+				password = line[15:-1]
+			else :
+				if options.be_verbose :
+					print "  -- WARNING: bad line in file '%s': %s" % (filename, line)
+				return (None,None)
+		
+		if (login is None) or (password is None) :
+			if options.be_verbose :
+				print "  -- WARNING: no 'OCDE_LOGIN' or 'OCDE_PASSWORD' lines in file '%s'" % (filename)
+			return (None,None)
+		
+		return (login, password)
+		
+	except IOError, e :
+		if options.be_verbose :
+			print "  -- WARNING: unable to parse file '%s'" % (filename)
+			print "  -- ", e
+		return (None,None)
+	except Exception, e :
+		if options.be_verbose :
+			print "  -- WARNING: unable to parse file '%s'" % (filename)
+			print "  -- ", e
+		return (None,None)
+
+
+def parse_queries( data ) :
+	res = []
+	
+	for m in re.finditer( r'<a href="search.php\?queryid=([0-9]*)">([^<]+)</', data ) :
+		res.append( m.groups() )
+	
+	return res
+
+def get_queries( login, password, cookies_filename ) :
+	if os.path.isfile( cookies_filename ) :
+		if options.be_verbose :
+			print "  -- loading cookies from '%s'" % cookies_filename
+		
+		try :
+			cookie_jar.load( cookies_filename )
+			
+			if options.be_verbose :
+				print "  -- trying to login via cookies"
+			
+			url = 'http://www.opencaching.de/query.php'
+			data = None
+			
+			try :
+				req = urllib2.Request( url, data, headers )
+				handle = urllib2.urlopen( req )
+			except IOError, e :
+				print "  -- failed to open '%s'" % url
+				print "  --", e
+				return None 
+			page = handle.read()
+			if 'resource2/ocstyle/images/misc/32x32-searchresults.png' in page :
+				if options.be_verbose :
+					print "  -- login via cookies successful"
+				return parse_queries( page )
+			else :
+				if options.be_verbose :
+					print "  -- login via cookies failed"
+				cookie_jar.clear()
+		except cookielib.LoadError, e :
+			print "  -- WARNING: failed to load cookies from '%s'" % cookies_filename
+			print "  --", e
+		 
+	if options.be_verbose :
+		print "  -- trying login via password"
+	
+	if login is None or password is None :
+		print "ERROR: username and/or password not specified"
+		raise Exception
+		return None
+		 
+	url = 'http://www.opencaching.de/login.php'
+	data = urllib.urlencode( { 'action' : 'login',
+							   'target' : 'query.php',
+							   'email' : login,
+							   'password' : password } )
+	
+	try:
+		req = urllib2.Request( url, data, headers )
+		handle = urllib2.urlopen( req )
+	except IOError, e:
+		print "ERROR: failed to open '%s'" % login_url
+		print "  -- ", e
+		raise Exception
+		return None
+	
+	page = handle.read()
+	if 'resource2/ocstyle/images/misc/32x32-searchresults.png' in page :
+		if options.be_verbose :
+			print "  -- login via password successful"
+			print "  -- storing cookies in '%s'" % cookies_filename
+		
+		try :
+			cookie_jar.save( cookies_filename );
+		except IOError, e :
+			print "  -- WARNING: failed to store cookies in '%s'" % cookies_filename
+			print "  --", e
+		
+		return parse_queries( page )
+	else :
+		print "ERROR: login via password failed"
+		raise Exception
+		return None
+
+
+def fancy_name( index, name, naming_style ) :
+	d = date.today().isoformat()
+	s = name.replace( '\\', ' ' ).replace( ' ', '_' )
+	if naming_style == 'ID' :
+		return index
+	elif naming_style == 'ID+DATE' :
+		return "%s_%s" % ( index, d )
+	elif naming_style == 'NAME' :
+		return s
+	elif naming_style == 'NAME+DATE' :
+		return "%s_%s" % ( s, d )
+	else :
+		return index
+
+
+def download_query( index, name, naming_style, target_dir ) :
+	create_directory( target_dir )
+	
+	target = "%s/%s.zip" % ( target_dir, fancy_name( index, name, naming_style ) )
+	
+	if options.be_verbose :
+		print "  -- downloading %s/'%s' as %s" % ( index, name, target )
+	
+	url = 'http://www.opencaching.de/search.php?queryid=%s&output=gpx&count=max&zip=1' % index
+	data = None
+	
+	# Open the url
+	try :
+		req = urllib2.Request( url, data, headers )
+		
+		if options.be_verbose :
+			print "  -- downloading " + url
+		handle = urllib2.urlopen( req )
+
+		local_file = open( target, "w" )
+		local_file.write( handle.read() )
+		local_file.close()
+
+	#handle errors
+	except urllib2.HTTPError, e :
+		print "  -- WARNING: failed to download '%s'" % url
+		print "  --", e
+	except urllib2.URLError, e :
+		print "  -- WARNING: failed to download '%s'" % url
+		print "  --", e
+	except IOError, e :
+		print "  -- WARNING: failed to download '%s' to file '%s'" % ( url, target )
+		print "  --", e
+
+
+	
+
+try :
+	config_dir = os.path.expanduser( options.config_dir )
+	if options.be_verbose :
+		print "  -- config_path='%s'" % (config_dir)
+	
+	create_directory( config_dir )
+	
+	(login,password) = load_credentials( config_dir + "/config.txt" )
+	cookies_filename=config_dir + "/cookies.txt"
+	
+	QUERIES = get_queries( login, password, cookies_filename )
+	
+	if options.list_only :
+		if options.be_verbose :
+			print "  -- available queries"
+		for (index, name) in QUERIES :
+			print "%s/'%s'" % ( index, name )
+	else :
+		if args == [] :
+			if options.be_verbose :
+				print "  -- downloading all queries"
+			for (index, name) in QUERIES :
+				download_query( index, name, options.naming_style, options.target_dir )
+		else :
+			if options.be_verbose :
+				print "  -- downloading specified queries"
+			
+			for aindex in args :
+				found = False
+				for (index, name) in QUERIES :
+					if aindex == index :
+						download_query( index, name, options.naming_style, options.target_dir )
+						found = True
+						break
+				if not found :
+					print "  -- WARNING: specifyed query with index '%s' not found" % aindex
+except Exception, e :
+	print "too bad, ocdl.py aborted due to an error!"
